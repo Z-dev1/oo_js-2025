f0c12bb4e1e462f2d9b8e147ea862a366d7fd47f
Z-dev1 <pedrodealmeida905@gmail.com>
2025-01-06 20:16:44 -0300

    oo_js 2025

---
+++ oo.js
@@ -0,0 +1,34 @@
+// Classe de abstração
+class Animal {
+    constructor(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+    }
+
+    emitirSom() {
+    console.log(`${this.nome} está emitindo um som.`);
+    }
+}
+
+  // Classes herdeiras
+class Cachorro extends Animal {
+    emitirSom() {
+    console.log(`${this.nome} está latindo.`);
+    }
+}
+
+class Gato extends Animal {
+    emitirSom() {
+    console.log(`${this.nome} está miando.`);
+    }
+}
+
+  // Instâncias de objetos
+const cachorro1 = new Cachorro("Rex", 3);
+const gato1 = new Gato("Mia", 2);
+const cachorro2 = new Cachorro("Max", 5);
+
+  // Testando as instâncias
+  cachorro1.emitirSom(); // Rex está latindo.
+  gato1.emitirSom();     // Mia está miando.
+  cachorro2.emitirSom(); // Max está latindo.
