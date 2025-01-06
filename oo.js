da0638d06e544587832ad8822da82334b7d33ee2
Z-dev1 <pedrodealmeida905@gmail.com>
2025-01-06 20:19:14 -0300

    Add files via upload

--- oo.js
+++ oo.js
@@ -23,12 +23,12 @@ class Gato extends Animal {
     }
 }
 
-  // Instâncias de objetos
+// Instâncias de objetos
 const cachorro1 = new Cachorro("Rex", 3);
 const gato1 = new Gato("Mia", 2);
 const cachorro2 = new Cachorro("Max", 5);
 
-  // Testando as instâncias
-  cachorro1.emitirSom(); // Rex está latindo.
-  gato1.emitirSom();     // Mia está miando.
-  cachorro2.emitirSom(); // Max está latindo.
+// Testando as instâncias
+cachorro1.emitirSom(); // Stam está latindo.
+gato1.emitirSom();     // Mimi está miando.
+cachorro2.emitirSom(); // Caramelo está latindo.
